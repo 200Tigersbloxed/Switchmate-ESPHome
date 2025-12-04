ead524df11909d70d987f1efd248a41239db0151
200Tigersbloxed <45884377+200Tigersbloxed@users.noreply.github.com>
2025-12-04 12:55:59 -0500

    python imports, schema, and namespace (hopefully)

--- components/switchmate/__init__.py
+++ components/switchmate/__init__.py
@@ -1,18 +1,49 @@
-from esphome.components import ble_client
+from esphome.components import ble_client, switch, sensor
 import esphome.config_validation as cv
 import esphome.codegen as cg
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_NAME
+
+from .switchmate import (
+    SwitchmateController,
+    SwitchmateSwitch
+)
 
 AUTO_LOAD = ["sensor", "switch", "ble_client"]
 
-switchmate_ns = cg.esphome_ns.namespace("switchmate")
+CONFIG_SCHEMA = (
+    switch.SWITCH_SCHEMA.extend({
+        cv.GenerateID(): cv.declare_id(SwitchmateController),
+        cv.Required("ble_client_id"): cv.use_id(ble_client.BLEClient),
+        cv.Optional("notify", default=False): cv.boolean,
+        cv.Optional("update_interval", default="1s"): cv.update_interval,
+    })
+)
 
-SwitchmateController = switchmate_ns.class_("SwitchmateController", cg.PollingComponent, ble_client.BLEClientNode)
+@switch.register_switch("switchmate", SwitchmateSwitch, CONFIG_SCHEMA)
+async def to_code(config):
+    var = cg.new_Pvariable(
+        config[CONF_ID],
+        config[CONF_NAME],
+        config["notify"],
+        config["update_interval"]
+    )
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(SwitchmateController),
-    cv.Required("ble_client_id"): cv.use_id(ble_client.BLEClient),
-    cv.Required("name"): cv.string,
-    cv.Optional("notify", default=False): cv.boolean,
-    cv.Optional("update_interval", default="1s"): cv.update_interval,
-})
+    await cg.register_component(var, config)
+
+    client = await cg.get_variable(config["ble_client_id"])
+    cg.add(client.register_ble_node(var))
+
+    # Battery sensor
+    batt = await sensor.new_sensor({
+        "name": f"{config[CONF_NAME]} Battery",
+        "icon": "mdi:battery",
+        "unit_of_measurement": "%",
+    })
+    cg.add(var.set_battery_sensor(batt))
+
+    # State switch
+    sw = await switch.new_switch({
+        "name": config[CONF_NAME],
+        "icon": "mdi:light-switch",
+    })
+    cg.add(var.set_state_switch(sw))
